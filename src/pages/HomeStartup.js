83ebec6cd5679edd39efc4181a6ad5499f646027
Divyang <divyangradadiya03@gmail.com>
2023-05-24 18:40:58 +0530

    Done

--- src/pages/HomeStartup.js
+++ src/pages/HomeStartup.js
@@ -1,65 +1,75 @@
-import React from 'react';
-import FooterOne from '../common/footer/FooterOne';
-import HeaderOne from '../common/header/HeaderOne';
-import SEO from '../common/SEO';
-import BannerFour from '../component/banner/BannerFour';
-import BlogOne from '../component/blog/BlogOne';
-import BrandOne from '../component/brand/BrandOne';
-import CtaLayoutOne from '../component/cta/CtaLayoutOne';
-import ProjectFour from '../component/project/ProjectFour';
-import TestimonialOne from '../component/testimonial/TestimonialOne';
-import ColorSwitcher from '../elements/switcher/ColorSwitcher';
-import ProcessOne from '../component/process/ProcessOne';
-
-import SectionTitle from '../elements/section-title/SectionTitle';
-import ServicePropOne from '../component/service/ServicePropOne';
-import CounterUpOne from '../component/counterup/CounterUpOne';
-
-
+import React from "react";
+import FooterOne from "../common/footer/FooterOne";
+import HeaderOne from "../common/header/HeaderOne";
+import SEO from "../common/SEO";
+import BannerFour from "../component/banner/BannerFour";
+import BlogOne from "../component/blog/BlogOne";
+import CtaLayoutOne from "../component/cta/CtaLayoutOne";
+import ProjectFour from "../component/project/ProjectFour";
+import TestimonialOne from "../component/testimonial/TestimonialOne";
+import ColorSwitcher from "../elements/switcher/ColorSwitcher";
+import ProcessOne from "../component/process/ProcessOne";
+import SectionTitle from "../elements/section-title/SectionTitle";
+import ServicePropOne from "../component/service/ServicePropOne";
+import CounterUpOne from "../component/counterup/CounterUpOne";
 
 const HomeStartup = () => {
-
-    return (
-        <>
-        <SEO title="Home Startup"/>
-        <ColorSwitcher />
-        <main className="main-wrapper">
+  return (
+    <>
+      <SEO title="Home Startup" />
+      <ColorSwitcher />
+      <main className="main-wrapper">
         <HeaderOne />
         <BannerFour />
         <ProjectFour />
         <TestimonialOne />
         <div className="section section-padding-2 bg-color-dark">
-                <div className="container">
-                    <SectionTitle 
-                        subtitle="What We Can Do For You"
-                        title="Services we can help you with"
-                        description="Nulla facilisi. Nullam in magna id dolor 
+          <div className="container">
+            <SectionTitle
+              subtitle="What We Can Do For You"
+              title="Services we can help you with"
+              description="Nulla facilisi. Nullam in magna id dolor 
                         blandit rutrum eget vulputate augue sed eu imperdiet."
-                        textAlignment="heading-light-left"
-                        textColor=""
-                    />
-                    <div className='row'>
-                        <ServicePropOne colSize="col-xl-4 col-md-6" serviceStyle="" itemShow="6" />
-                    </div>
-                </div>
-                <ul className="list-unstyled shape-group-10">
-                    <li className="shape shape-1"><img src={process.env.PUBLIC_URL + "/images/others/line-9.png"} alt="Circle" /></li>
-                    <li className="shape shape-2"><img src={process.env.PUBLIC_URL + "/images/others/bubble-42.png"} alt="Circle" /></li>
-                    <li className="shape shape-3"><img src={process.env.PUBLIC_URL + "/images/others/bubble-43.png"} alt="Circle" /></li>
-                </ul>
+              textAlignment="heading-light-left"
+              textColor=""
+            />
+            <div className="row">
+              <ServicePropOne
+                colSize="col-xl-4 col-md-6"
+                serviceStyle=""
+                itemShow="6"
+              />
             </div>
-        {/* <BrandOne /> */}
+          </div>
+          <ul className="list-unstyled shape-group-10">
+            <li className="shape shape-1">
+              <img
+                src={process.env.PUBLIC_URL + "/images/others/line-9.png"}
+                alt="Circle"
+              />
+            </li>
+            <li className="shape shape-2">
+              <img
+                src={process.env.PUBLIC_URL + "/images/others/bubble-42.png"}
+                alt="Circle"
+              />
+            </li>
+            <li className="shape shape-3">
+              <img
+                src={process.env.PUBLIC_URL + "/images/others/bubble-43.png"}
+                alt="Circle"
+              />
+            </li>
+          </ul>
+        </div>
         <BlogOne />
-
         <CounterUpOne />
-
         <CtaLayoutOne />
-        <ProcessOne/>
+        <ProcessOne />
         <FooterOne />
-        </main>
-        </>
-    )
-}
+      </main>
+    </>
+  );
+};
 
 export default HomeStartup;
-
